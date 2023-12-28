10a05fb285218a13e1874eeb8991359096ec75e7
Maksim Aniskov <MaksimAniskov@gmail.com>
2023-12-28 19:20:10 +0100

    Use recommended .cypher extension
    See https://neo4j.com/docs/cypher-manual/current/styleguide/#cypher-styleguide-general-recommendations

--- neo4j/archi-verifier.sh
+++ neo4j/archi-verifier.sh
@@ -2,6 +2,6 @@
 
 cp -r /usr/var/verification_scripts import/verification_scripts
 
-cypher-shell -f import-archi-model.cyp
+cypher-shell -f import-archi-model.cypher
 
 find import/verification_scripts -type f | sort | xargs -L1 -I {} cypher-shell -f "{}"
