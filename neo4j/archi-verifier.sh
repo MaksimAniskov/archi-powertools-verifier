dfc73b78b7b3e55f5ccd1bcbba37c67b24d86838
Maksim Aniskov <MaksimAniskov@gmail.com>
2023-12-28 19:17:03 +0100

    Strictly order execution of verification scripts

--- neo4j/archi-verifier.sh
+++ neo4j/archi-verifier.sh
@@ -4,4 +4,4 @@ cp -r /usr/var/verification_scripts import/verification_scripts
 
 cypher-shell -f import-archi-model.cyp
 
-find import/verification_scripts -type f -exec cypher-shell -f {} \;
+find import/verification_scripts -type f | sort | xargs -L1 -I {} cypher-shell -f "{}"
