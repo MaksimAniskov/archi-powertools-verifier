bc9ee44eacd9a79d7ffb436ecaf99245c618f5e7
Maksim Aniskov <MaksimAniskov@gmail.com>
2023-12-30 15:33:53 +0100

    Show verification progress

--- neo4j/archi-verifier.sh
+++ neo4j/archi-verifier.sh
@@ -2,6 +2,10 @@
 
 cp -r /usr/var/verification_scripts import/verification_scripts
 
+echo "Importing the Archimate model to Neo4j..."
 cypher-shell -f import-archi-model.cypher
 
-find import/verification_scripts -type f | sort | xargs -L1 -I {} cypher-shell -f "{}"
+echo "Executing verification scripts..."
+find import/verification_scripts -type f | sort | xargs -L1 -I {} sh -c 'echo "{}"; cypher-shell -f "{}"'
+
+echo "Verification completed"
