078a12c1df6325531f86cbd63c7825eec6d8c81b
Maksim Aniskov <MaksimAniskov@gmail.com>
2024-01-03 20:49:43 +0100

    Map additional data folder in read-only mode to the container

--- neo4j/archi-verifier.sh
+++ neo4j/archi-verifier.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp -r /usr/var/verification_scripts import/verification_scripts
+cp -r /usr/var/additional_data import/additional_data
 
 echo "Importing the Archimate model to Neo4j..."
 cypher-shell -f import-archi-model.cypher
